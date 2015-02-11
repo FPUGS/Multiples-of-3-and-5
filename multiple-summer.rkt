eb5a1c545aee94e994716b62e5a5ea67deda19c3
Trevor Alexander <talex@privatdemail.net>
2015-02-10 17:30:05 -1000

    draft func, pseudocode

---
+++ multiple-summer.rkt
@@ -0,0 +1,19 @@
+#lang racket
+
+(provide multiple-summer)
+
+;@ the plan:
+;@ . curry a modulo for every factor in multlist (O(n))
+;@ . filter by calling each of the curried funcs on each of the items (O(mn))
+;@ . sum the filtered resulting list (O(n))
+;@ functional so this will be backwards
+
+(define (multiple-summer highnum [multlist [3 5]])
+  ;@ problem: this doesn't unquote its args
+  (define modfuncs (map (lambda x ;@ make some modulos
+                          (curry modulo x)) multlist))
+  
+  (+
+   
+   )
+  )
