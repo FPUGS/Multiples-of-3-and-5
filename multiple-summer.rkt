c37c6ba83d6b4f8d0ac268778560367e5a96bb8b
Trevor Alexander <talex@privatdemail.net>
2015-02-11 12:20:59 -1000

    testing without optional args

--- multiple-summer.rkt
+++ multiple-summer.rkt
@@ -9,16 +9,21 @@
 ;@ . sum the filtered resulting list (O(n))
 ;@ functional so this will be backwards
 
-(define (multiple-summer highnum [multlist [3 5]])
+(define (multiple-summer highnum) ; [multlist '(3 5)])
+  (define multlist '(3 5))
   ;@ make some modulos
   (define modfuncs (map (lambda (x)
                           (curry modulo x)) multlist))
 
   (+ (sequence-filter
    (lambda (thisnum)
-     (equal? (+ (map modfuncs thisnum)) 0)) ;@ map each curried func to thisnum
+     (equal? (for/sum ([thismod modfuncs])
+               (thismod thisnum)) ;@ run each curried func on thisnum, sum
+                                  ;result
+             0)
    (in-range 1 highnum)) ;@ generated sequence
      ) ;@ sum the numbers we got
   )
+)
 
 ;@ (for/sum ([thismod modfuncs]) (thismod thisnum))
