d07a10f8d7d1847460ef75a7218bb270286a25f8
Trevor Alexander <talex@privatdemail.net>
2015-07-31 20:07:04 -1000

    fixed multiple-summer based on successful multiple-printer

--- multiple-summer.rkt
+++ multiple-summer.rkt
@@ -1,32 +1,35 @@
 #lang racket
-
 (require racket/sequence)
 (provide multiple-summer)
 
-;@ the plan:
-;; . curry a modulo for every factor in multlist (O(n))
-;; . filter by calling each of the curried funcs on each of the items (O(mn))
-;; . sum the filtered resulting list (O(n))
-;; functional so this will be backwards
+;@ A function that maps each of the functions in fs to each of the of the inputs
+; in xs.
+(define (tensor-map fs xs) (map (λ (f) (map f xs)) fs))
 
-(define (multiple-summer highnum) ; [multlist '(3 5)])
-  (define multlist '(3 5))
-  ;@ make some modulos
-  (define modfuncs (map (lambda (x)
-                          (curryr modulo x)) multlist))
+;@ A function that computes modulos of different moduli to different arguments.
+(define (multiple-modulos modulos numargs)
+  ; Create single-input curried modulos to apply to each of the args
+  (define modfuncs (map (lambda (x) (curryr modulo x)) modulos))
+  (tensor-map modfuncs numargs))
 
-  (sequence-fold + 0
-                 (sequence-filter
-                  (lambda (thisnum)
-                    (equal? (apply-funcs-sum modfuncs thisnum) 0))
-                  (in-range 1 highnum)) ;; generated sequence
-                 ) ;; sum the numbers we got
-  )
+;@ A function that takes a list of two lists and zips the two lists together
+; into a longer one.
+(define (zip-2 listoflists)
+  (car (map list (car listoflists) (cadr listoflists))))
 
-(define (apply-funcs-sum funclist onearg) ;@ applies a list of functions to an
-                                          ;; argument, instead of the other way
-                                          ;; around, then sums results
-  (for/sum ([thisfunc funclist])
-    (displayln onearg)
-    (thisfunc onearg))
-  )
+;@ finally the main function. Takes as input the higher number N in the range
+; from 1 to N.
+(define (multiple-summer highnum)
+  (apply + (filter (lambda (seqnum) ; since we need the sequence, map over it
+         ; local binding for the modulos of that sequence val
+         (let ([modpair (zip-2 (multiple-modulos '(3 5) (list seqnum)))])
+           (cond ; three conditions testing the modulo result
+             [(= (car modpair) 0) #t]
+             [(= (cadr modpair) 0) #t]
+             [else #f])))
+             ; convert sequence to list so map can be called. Sequences have
+             ; lots of nice properties but here I just want to make an
+             ; increasing list of numbers without typing too much.
+             (sequence->list (in-range 1 (add1 highnum))))))
+
+(multiple-summer 1000)
