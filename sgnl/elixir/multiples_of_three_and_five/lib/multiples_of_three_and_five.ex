7d882042ce46b1d627f00cf57c3edfe981d84142
sgnl <rayrfarias@gmail.com>
2015-10-03 00:34:33 -1000

    refactor multi module to work with Enum.reduce/3

--- sgnl/elixir/multiples_of_three_and_five/lib/multiples_of_three_and_five.ex
+++ sgnl/elixir/multiples_of_three_and_five/lib/multiples_of_three_and_five.ex
@@ -7,22 +7,11 @@ defmodule MultiplesOfThreeAndFive do
         0
       true ->
         # cast n to list and recurse with 0 as initial accumulator value
-        multi(Enum.to_list(1 .. n - 1 ), 0)
-    end
-  end
-
-  # base case, if given an empty list, return the accumulator
-  def multi([], sum), do: sum
-
-  # check for multiple of 3, recurse
-  def multi([first|rest], sum) do
-    cond do
-      rem(first, 3) == 0 ->
-        multi(rest, first + sum)
-      rem(first, 5) == 0 ->
-        multi(rest, first + sum)
-      true ->
-        multi(rest, sum)
+        Enum.reduce(Enum.to_list(1 .. n - 1 ), 0,
+          fn(x, acc) ->
+            if rem(x, 3) == 0 || rem(x, 5) == 0, do: x + acc, else: acc
+          end
+        )
     end
   end
 
