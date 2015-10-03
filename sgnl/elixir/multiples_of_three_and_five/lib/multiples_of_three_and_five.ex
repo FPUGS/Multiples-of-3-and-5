24d5eca66ba85b1ca94c82aef455943c66c5af24
sgnl <rayrfarias@gmail.com>
2015-10-03 00:24:01 -1000

    implements main functionality

--- sgnl/elixir/multiples_of_three_and_five/lib/multiples_of_three_and_five.ex
+++ sgnl/elixir/multiples_of_three_and_five/lib/multiples_of_three_and_five.ex
@@ -1,3 +1,29 @@
 defmodule MultiplesOfThreeAndFive do
-  def multi(n) when n < 3, do: 0
+
+  def multi(n) do
+    cond do
+      # return zero if given a number less than 3
+      n < 3 ->
+        0
+      true ->
+        # cast n to list and recurse with 0 as initial accumulator value
+        multi(Enum.to_list(1 .. n - 1 ), 0)
+    end
+  end
+
+  # base case, if given an empty list, return the accumulator
+  def multi([], sum), do: sum
+
+  # check for multiple of 3, recurse
+  def multi([first|rest], sum) do
+    cond do
+      rem(first, 3) == 0 ->
+        multi(rest, first + sum)
+      rem(first, 5) == 0 ->
+        multi(rest, first + sum)
+      true ->
+        multi(rest, sum)
+    end
+  end
+
 end
